34567016e42a4f44ba3cc469dcd14332771965e2
AhmedSalah172002 <ahmed172002tayel@gmail.com>
2025-02-09 10:22:50 +0200

    check actions

--- tests/CircleAreaTest.php
+++ tests/CircleAreaTest.php
@@ -2,7 +2,7 @@
 
 use AhmedSalah172002\CircleArea\CircleArea;
 
-it('can get circle area in correctly way', function () {
+it('can get circle area correctly', function () {
 
     $area = CircleArea::Raduis(5)->getArea();
 
